331f3b15090249c6b392842fe3d980ae57a516f2
Dan <dpb34@pitt.edu>
2019-08-26 18:20:33 -0400

    update package

--- src/Sample/Sample.js
+++ src/Sample/Sample.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./Sample.css";
-import { ViewDriver, Fretboard, Keyboard, TheoryEngine, Concepts } from "C://Users/dan94/Desktop/play-what-alpha/build/play-what-alpha";
+import { Fretboard, Keyboard, TheoryEngine, INTERVAL_PAIR, INTERVAL, CHORD, SCALE, MODE } from "C://Users/dan94/Desktop/play-what-alpha/build/play-what-alpha";
 
 export class Sample extends React.Component {
 
@@ -11,60 +11,8 @@ export class Sample extends React.Component {
     render() {
         return (
             <div className="sample-container">
-                <ViewDriver />
-
-                <Fretboard
-                    notes={
-                        TheoryEngine.parseIntervals(
-                            {
-                                degree: {
-                                    id: 'C',
-                                    name: 'C',
-                                    value: 1,
-                                    index: 0
-                                },
-                                accidental: {
-                                    id: 'natural',
-                                    name: '♮',
-                                    offset: 0
-                                },
-                                octave: 4
-                            },
-                            Concepts.Chords.maj.intervals,
-                            {}
-                        )}
-                    config={{
-                        noteLabel: {
-                            id: 'name',
-                            name: 'Name'
-                        },
-                        showDots: true,
-                        filterOctave: true,
-                        fretLow: 0,
-                        fretHigh: 12,
-                        strings: [
-                            { openPosition: 16 },   // e
-                            { openPosition: 11 },   // B
-                            { openPosition: 7 },    // G
-                            { openPosition: 2 },    // D
-                            { openPosition: -3 },   // A
-                            { openPosition: -8 }    // E   
-                        ]
-                    }}
-                />
-
-                <Keyboard
-                    notes={[]}
-                    config={{
-                        noteLabel: {
-                            id: 'interval',
-                            name: 'Interval'
-                        },
-                        filterOctave: true,
-                        keyLow: 0,
-                        keyHigh: 24
-                    }}
-                />
+                <Fretboard/>
+                <Keyboard/>
             </div>
         )
     }
