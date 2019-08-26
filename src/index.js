e2ac3dfb150bb1a110b5c44a0c0d5f7f4ca55407
Dan <dpb34@pitt.edu>
2019-08-26 18:52:27 -0400

    basic intro text

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Sample } from "./Sample/Sample";
+import "./index.css"
 
 ReactDOM.render(<Sample />, document.querySelector("#app"));
