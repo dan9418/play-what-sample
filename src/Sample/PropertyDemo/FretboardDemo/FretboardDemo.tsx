f1caa2f88b36ea3f2c92c35a5098a7c269ecfbe9
Dan <dpb34@pitt.edu>
2019-08-28 13:17:43 -0400

    add fretboard demo

---
+++ src/Sample/PropertyDemo/FretboardDemo/FretboardDemo.tsx
@@ -0,0 +1,97 @@
+import './FretboardDemo.css';
+import { Fretboard, FretboardProps, DEFAULT_FRETBOARD_PROPS, NOTE_LABEL } from 'C://Users/dan94/Desktop/play-what-alpha/build/play-what-alpha';
+import React = require('react');
+import { KeyCenterInput } from '../KeyCenterProperties/KeyCenterProperties';
+import { ConceptInput } from '../ConceptProperties/ConceptProperties';
+import { EnumDropdownInput } from '../../Inputs/EnumDropdownInput/EnumDropdownInput';
+import { BooleanInput } from '../../Inputs/BooleanInput/BooleanInput';
+import { NumericInput } from '../../Inputs/NumericInput/NumericInput';
+import { Property } from '../Property/Property';
+
+export enum FretboardProp {
+    keyCenter = 'keyCenter',
+    concept = 'concept',
+    filterOctave = 'filterOctave',
+    noteLabel = 'noteLabel',
+    fretLow = 'fretLow',
+    fretHigh = 'fretHigh'
+}
+
+type FretboardDemoProps = {
+    customProps: FretboardProps;
+    showProps: FretboardProp[];
+}
+
+export class FretboardDemo extends React.Component<FretboardDemoProps, FretboardProps> {
+
+    constructor(props) {
+        super(props);
+        this.state = Object.assign({}, DEFAULT_FRETBOARD_PROPS, this.props.customProps);
+    }
+
+    setValue = (property: string, value: any) => {
+        let update = {};
+        update[property] = value;
+        this.setState(update);
+    }
+
+    showProp = (prop: FretboardProp) => {
+        return this.props.showProps.indexOf(prop) !== -1;
+    }
+
+    render() {
+        return (
+            <div className='demo'>
+                <div className='demo-pre'>
+                    <div className='demo-text'>{'<Fretboard'}</div>
+                    {this.showProp(FretboardProp.keyCenter) &&
+                        <Property label={FretboardProp.keyCenter} nested={true}>
+                            <KeyCenterInput
+                                value={this.state[FretboardProp.keyCenter]}
+                                setValue={(value: any) => this.setValue(FretboardProp.keyCenter, value)}
+                            />
+                        </Property>}
+                    {this.showProp(FretboardProp.concept) &&
+                        <Property label={FretboardProp.concept} nested={true}>
+                            <ConceptInput
+                                value={this.state[FretboardProp.concept]}
+                                setValue={(value: any) => this.setValue(FretboardProp.concept, value)}
+                            />
+                        </Property>}
+                    {this.showProp(FretboardProp.noteLabel) &&
+                        <Property label={FretboardProp.noteLabel}>
+                            <EnumDropdownInput
+                                value={this.state[FretboardProp.noteLabel]}
+                                setValue={(value: any) => this.setValue(FretboardProp.noteLabel, value)}
+                                label='NOTE_LABEL'
+                                enum={NOTE_LABEL}
+                            />
+                        </Property>}
+                    {this.showProp(FretboardProp.filterOctave) &&
+                        <Property label={FretboardProp.filterOctave}>
+                            <BooleanInput
+                                value={this.state[FretboardProp.filterOctave]}
+                                setValue={(value: any) => this.setValue(FretboardProp.filterOctave, value)}
+                            />
+                        </Property>}
+                    {this.showProp(FretboardProp.fretLow) &&
+                        <Property label={FretboardProp.fretLow}>
+                            <NumericInput
+                                value={this.state[FretboardProp.fretLow]}
+                                setValue={(value: any) => this.setValue(FretboardProp.fretLow, value)}
+                            />
+                        </Property>}
+                    {this.showProp(FretboardProp.fretHigh) &&
+                        <Property label={FretboardProp.fretHigh}>
+                            <NumericInput
+                                value={this.state[FretboardProp.fretHigh]}
+                                setValue={(value: any) => this.setValue(FretboardProp.fretHigh, value)}
+                            />
+                        </Property>}
+                    <div className='keyboard-demo-text'>{'/>'}</div>
+                </div>
+                <Fretboard {...this.state} />
+            </div>
+        )
+    }
+}
--- src/Sample/Sample.tsx
+++ src/Sample/Sample.tsx
@@ -2,6 +2,7 @@ import "./Sample.css";
 import { Fretboard, Keyboard, DEGREE, ACCIDENTAL, NOTE_LABEL, TheoryEngine, INTERVAL_PAIR, INTERVAL, CHORD, SCALE, MODE } from "C://Users/dan94/Desktop/play-what-alpha/build/play-what-alpha";
 import React = require("react");
 import { KeyboardDemo, KeyboardProp } from "./PropertyDemo/KeyboardDemo/KeyboardDemo";
+import { FretboardDemo, FretboardProp } from "./PropertyDemo/FretboardDemo/FretboardDemo";
 
 export class Sample extends React.Component<any, any> {
 
@@ -12,6 +13,11 @@ export class Sample extends React.Component<any, any> {
     render() {
         return (
             <div className="sample-container">
+
+                <FretboardDemo
+                    customProps={{}}
+                    showProps={[FretboardProp.keyCenter]}
+                />
 
                 <KeyboardDemo
                     customProps={{}}
@@ -31,81 +37,19 @@ export class Sample extends React.Component<any, any> {
                 <p>Currently, Play What provides two viewer components out-of-the-box: a keyboard and a fretboard.
                     Without providing any custom configuration, they will render as follows:</p>
 
-                <pre>
-                    {"<Fretboard/>"}
-                </pre>
-
-                <Fretboard />
-
-                <pre>
-                    {"<Keyboard/>"}
-                </pre>
-
-                <Keyboard />
-
-
                 <p>To apply a music theory concept to a viewer, simply provide one via props.
                     There are built-in presets for chords, scales, modes, roman numerals, and interval pairs.
                     A complete list can be found in the documentation. By default, the notes are labeled with their respective interval and color-coded by degree.</p>
-
-                <pre>{
-                    `<Keyboard
-    concept={{intervals: CHORD.Maj.intervals}}
-/>`
-                }</pre>
-
-                <Keyboard
-                    concept={{ intervals: CHORD.Maj.intervals }}
-                />
 
                 <h3>Modifying a Concept</h3>
 
                 <p>By default, concepts are assumed to be in the key of C in the octave of Middle C. To change the key, provide a value for the keyCenter prop.</p>
 
-                <pre>{
-                    `<Keyboard
-                    keyCenter={{ degree: DEGREE.G, accidental: ACCIDENTAL.Flat, octave: 5 }}
-                    concept={{ intervals: CHORD.Maj.intervals }}
-                />`
-                }</pre>
-
-                <Keyboard
-                    keyCenter={{ degree: DEGREE.G, accidental: ACCIDENTAL.Flat, octave: 5 }}
-                    concept={{ intervals: CHORD.Maj.intervals }}
-                />
-
                 <p>There is also support for chordal inversions and melodic inversions.</p>
-
-                <pre>{
-                    `<Keyboard
-    keyCenter={{ degree: DEGREE.C, accidental: ACCIDENTAL.Natural, octave: 5 }}
-    concept={{ intervals: CHORD.Maj.intervals, intervalOptions: { chordInversion: 1 } }}
-/>`
-                }</pre>
-
-                <Keyboard
-                    keyCenter={{ degree: DEGREE.C, accidental: ACCIDENTAL.Natural, octave: 5 }}
-                    concept={{ intervals: CHORD.Maj.intervals, intervalOptions: { chordInversion: 1 } }}
-                />
-
-                <pre>{
-                    `<Keyboard
-    keyCenter={{ degree: DEGREE.C, accidental: ACCIDENTAL.Natural, octave: 5 }}
-    concept={{ intervals: CHORD.Maj.intervals, intervalOptions: { melodicInversion: true } }}
-/>`
-                }</pre>
-
-                <Keyboard
-                    keyCenter={{ degree: DEGREE.C, accidental: ACCIDENTAL.Natural, octave: 5 }}
-                    concept={{ intervals: CHORD.Maj.intervals, intervalOptions: { melodicInversion: true } }}
-                />
 
                 <h3>Configuring Viewers</h3>
 
                 <p>Each viewer also its own API for modifying how concepts are displayed. A complete list of all the props and values is available in the documentation.</p>
-
-
-
 
             </div >
         )
