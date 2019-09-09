e4b63c15f61baa3bc33837ca00d1263737e945f5
Dan Bednarczyk <dpbednarczyk@gmail.com>
2019-09-09 09:23:42 -0400

    Finalize reference

--- src/Pages/Reference.tsx
+++ src/Pages/Reference.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Demo } from "../Demo/Demo";
-import { TONIC, ACCIDENTAL, INTERVAL, CHORD, SCALE, MODE, ROMAN_NUMERAL, INTERVAL_PAIR, Fretboard, DEFAULT_FRETBOARD_PROPS, Keyboard, DEFAULT_KEYBOARD_PROPS } from 'play-what-beta';
+import { INTERVAL, CHORD, SCALE, MODE, ROMAN_NUMERAL, INTERVAL_PAIR } from 'play-what-beta';
 import { FormattedTable } from "../Common/FormattedTable/FormattedTable";
 import { IntervalTable } from "../Common/IntervalTable/IntervalTable";
 import { ConceptTable } from "../Common/ConceptTable/ConceptTable";
@@ -8,6 +7,42 @@ import { ConceptTable } from "../Common/ConceptTable/ConceptTable";
 export function Reference(props: any) {
     return (
         <div className='docs-section'>
+
+            <h1>The INTERVAL Constant</h1>
+
+            <p>
+                The <span className='inline-pre'>INTERVAL</span> constant is the central idea behind <span className='italic'>Play What's</span> computations.
+                Each property represents one musical interval.
+            </p>
+
+            <p>
+                A musical interval specifies the distance between two notes.
+                <br />
+                The distance is measured in two ways, <span className='italic'>degree</span> and <span className='italic'>semitones</span>.
+                <br />
+                Degree represents the difference in letter names (e.g. A to B).
+                <br />
+                Semitones represent the difference in pitch.
+                <br />
+                There are 12 semitones in an octave.
+            </p>
+
+            <p>
+                The table below shows the available <span className='inline-pre'>INTERVAL</span> values organized by degree (y) and semitones (x).
+            </p>
+
+            <IntervalTable intervals={(Object as any).values(INTERVAL)} />
+
+            {/*<FormattedTable
+                title='Interval Interface'
+                headers={['Prop', 'Type', 'Description']}
+                rows={[
+                    ['id', 'string', 'Unique identifier'],
+                    ['name', 'string', 'Display name'],
+                    ['degree', 'number', 'Difference in degrees'],
+                    ['semitones', 'number', 'Difference in semitones']
+                ]}
+            />*/}
 
             <h1>Enum Values</h1>
 
@@ -36,7 +71,7 @@ export function Reference(props: any) {
             />
 
             {/*<FormattedTable
-                title=''
+                title='Tonic Interface'
                 headers={['Prop', 'Type', 'Description']}
                 rows={[
                     ['id', 'string', 'Unique identifier'],
@@ -51,21 +86,21 @@ export function Reference(props: any) {
             <p>
                 An accidental offsets the pitch of a note.
                 <br />
-                It is represented by a symbol (generally b or #) appended to the note's letter name.
+                It is represented by a symbol appended to the note's letter name.
             </p>
 
             <FormattedTable
                 title='ACCIDENTAL'
-                headers={['Value', 'Description']}
+                headers={['Value', 'Symbol', 'Description']}
                 rows={[
-                    ['Natural', 'Does not alter the key tonic'],
-                    ['Flat', 'Lowers the key tonic by one semitone'],
-                    ['Sharp', 'Raises the key tonic by one semitone']
+                    ['Natural', '', 'Does not alter the note\'s pitch'],
+                    ['Flat', 'b', 'Lowers the note\'s pitch by one semitone'],
+                    ['Sharp', '#', 'Raises the note\'s pitch by one semitone']
                 ]}
             />
 
             {/*<FormattedTable
-                title=''
+                title='Accidental Interface'
                 headers={['Prop', 'Type', 'Description']}
                 rows={[
                     ['id', 'string', 'Unique identifier'],
@@ -74,41 +109,10 @@ export function Reference(props: any) {
                 ]}
             />*/}
 
-            <h2>Interval</h2>
-
-            <p>
-                A musical interval represents the distance between two notes.
-                <br />
-                The distance is measured in two ways, <span className='italic'>degree</span> and <span className='italic'>semitones</span>.
-                <br/>
-                Degree represents the difference in letter names (e.g. A to B).
-                <br />
-                Semitones represent the difference in pitch.
-                <br />
-                There are 12 semitones in an octave,
-            </p>
-
-            <p>
-                The table below shows the available <span className='inline-pre'>INTERVAL</span> enums organized by degree (y) and semitones (x).
-            </p>
-
-            <IntervalTable intervals={(Object as any).values(INTERVAL)} />
-
-            {/*<FormattedTable
-                title=''
-                headers={['Prop', 'Type', 'Description']}
-                rows={[
-                    ['id', 'string', 'Unique identifier'],
-                    ['name', 'string', 'Display name'],
-                    ['degree', 'number', 'Difference in degrees'],
-                    ['semitones', 'number', 'Difference in semitones']
-                ]}
-            />*/}
-
             <h2>Note Label</h2>
 
             <p>
-                The following enums can be used to represent the properties exposed by a <span className='inline-pre'>Note</span> object.
+                The following enums can be passed to a <span className='inline-pre'>keyLabel</span> or <span className='inline-pre'>fretLabel</span> prop.
             </p>
 
             <FormattedTable
@@ -116,13 +120,13 @@ export function Reference(props: any) {
                 headers={['Value', 'Description']}
                 rows={[
                     ['None', 'No label'],
-                    ['Name', 'The symbol of the notes degree and accidental e.g. A#, Gb...'],
-                    ['Interval', 'The interval of the note relative to the key tonic'],
-                    ['PitchClass', 'The number of semitones from the last key tonic'],
-                    ['NoteIndex', 'The number of semitones from Middle C'],
-                    ['RelativeDegree', 'The diatonic degree of the note'],
-                    ['Octave', 'The octave that the note belongs to'],
-                    ['Frequency', 'The pitch of the note in Hz']
+                    ['Name', 'Note name'],
+                    ['Interval', 'Interval name'],
+                    ['Degree', 'Interval degree'],
+                    ['PitchClass', 'Distance from C'],
+                    ['NoteIndex', 'Distance from Middle C'],
+                    ['Octave', 'Note octave'],
+                    ['Frequency', 'Note frequency']
                 ]}
             />
 
